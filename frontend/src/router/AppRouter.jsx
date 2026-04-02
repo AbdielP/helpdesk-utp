dbf96149445d99d5e1ef8cf38776d0adcaff2f8f
AbdielP <abdiel.pinzonc@gmail.com>
2026-04-02 17:09:57 -0500

    router basico

---
+++ frontend/src/router/AppRouter.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+        
+    // !!!!! COMPONENTES PENDIENTES POR CREAR
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/tickets" element={<div>Tickets</div>} />
+        <Route path="/tickets/new" element={<div>New Ticket</div>} />
+        <Route path="/admin" element={<div>Admin</div>} />
+      </Routes>
+    </BrowserRouter>
+
+    // TODO:
+    // 1) Navegacion
+    // 2) Rutas protegidas
+    // 3) Navbar y header
+  );
+}
+
+export default AppRouter;
