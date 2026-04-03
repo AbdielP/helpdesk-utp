a3bc48bf450efae217ef3c9d800517ffd70957dc
AbdielP <abdiel.pinzonc@gmail.com>
2026-04-03 08:34:00 -0500

    basic components

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DashboardPage } from "../Pages/DashboardPage";
 
 const AppRouter = () => {
   return (
-        
+
     // !!!!! COMPONENTES PENDIENTES POR CREAR
 
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<div>Login</div>} />
-        <Route path="/" element={<div>Dashboard</div>} />
+        <Route path="/" element={
+          <DashboardPage />
+        } />
         <Route path="/tickets" element={<div>Tickets</div>} />
         <Route path="/tickets/new" element={<div>New Ticket</div>} />
         <Route path="/admin" element={<div>Admin</div>} />
@@ -19,6 +22,7 @@ const AppRouter = () => {
     // 1) Navegacion
     // 2) Rutas protegidas
     // 3) Navbar y header
+    // 4) Estilos css basicos (margin, fontsize, color, etc)
   );
 }
 
