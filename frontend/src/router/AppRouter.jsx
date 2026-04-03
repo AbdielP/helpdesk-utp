50709245a2396f4a6f17f3320b12042b3fe0cb2b
AbdielP <abdiel.pinzonc@gmail.com>
2026-04-03 16:46:03 -0500

    render condicional navbar

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,16 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import { DashboardPage } from "../Pages/DashboardPage";
 import { Topbar } from '../components/Topbar';
+import LoginPage from "../Pages/LoginPage";
 
 const AppRouter = () => {
+  const location = useLocation();
+  const showTopbar = location.pathname !== "/login";
+
   return (
-
-    // !!!!! COMPONENTES PENDIENTES POR CREAR
-
-    <BrowserRouter>
-      <Topbar/>
+    <>
+      {showTopbar && <Topbar />}
       <Routes>
-        <Route path="/login" element={<div>Login</div>} />
+        <Route path="/login" element={
+          <LoginPage />
+        } />
         <Route path="/" element={
           <DashboardPage />
         } />
@@ -18,14 +21,7 @@ const AppRouter = () => {
         <Route path="/tickets/new" element={<div>New Ticket</div>} />
         <Route path="/admin" element={<div>Admin</div>} />
       </Routes>
-    </BrowserRouter>
-
-    // TODO:
-    // 0) Crear pages y componentes faltantes
-    // 1) Navegacion (en navbar y/o header)
-    // 2) Rutas protegidas (todas menos login)
-    // 3) Diseñar Navbar y header (visible en todas las paginas menos en login)
-    // 4) Estilos css basicos (margin, fontsize, color, etc)
+    </>
   );
 }
 
