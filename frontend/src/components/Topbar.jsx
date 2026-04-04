40975e5c60f3369d4ce7ca470fab3f3a7c987582
AbdielP <abdiel.pinzonc@gmail.com>
2026-04-04 18:58:39 -0500

    topbar sin styled

--- frontend/src/components/Topbar.jsx
+++ frontend/src/components/Topbar.jsx
@@ -1,51 +1,44 @@
-import MuiAppBar from '@mui/material/AppBar';
-import { styled } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import { DRAWER_WIDTH } from '../constants/constants';
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
-})(({ theme }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        width: `calc(100% - ${DRAWER_WIDTH}px)`,
-        marginLeft: `${DRAWER_WIDTH}px`,
-        transition: theme.transitions.create(['margin', 'width'], {
-          easing: theme.transitions.easing.easeOut,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
-    },
-  ],
-}));
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import { DRAWER_WIDTH } from "../constants/constants";
 
 const Topbar = ({ open, handleDrawerOpen }) => {
   return (
-    <AppBar position="fixed" open={open}>
+    <AppBar
+      position="fixed"
+      sx={{
+        transition: "margin 225ms, width 225ms",
+        ...(open
+          ? {
+              width: `calc(100% - ${DRAWER_WIDTH}px)`,
+              marginLeft: `${DRAWER_WIDTH}px`,
+            }
+          : {
+              width: "100%",
+              marginLeft: 0,
+            }),
+      }}
+    >
       <Toolbar>
         <IconButton
           size="large"
           edge="start"
           color="inherit"
           aria-label="menu"
-          sx={{ mr: 2 }}
+          sx={{ mr: 2, display: open ? "none" : "inline-flex" }}
           onClick={handleDrawerOpen}
         >
           <MenuIcon />
         </IconButton>
+
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           HelpDesk UTP
         </Typography>
+
         <IconButton
           size="large"
           aria-label="account of current user"
@@ -58,6 +51,6 @@ const Topbar = ({ open, handleDrawerOpen }) => {
       </Toolbar>
     </AppBar>
   );
-}
+};
 
-export default Topbar;  
+export default Topbar;
